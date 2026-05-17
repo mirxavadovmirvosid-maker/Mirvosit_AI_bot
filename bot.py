d45145ed512a0c8150d2385bc94dfb35867764b8
mirxavadovmirvosid-maker <mirxavadovmirvosid@gmail.com>
2026-05-17 23:41:41 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
-import telebot
-from google import genai
 import os
 from threading import Thread
+import telebot
+from google import genai
+from flask import Flask
 
 # SIZNING TELEGRAM BOT TOKENINGIZ
-BOT_TOKEN = "8917394328:AAHYp1VAZpMLltPbOEceE3-GsmX0OCh4uwY"
+BOT_TOKEN = "8917394328:AAHyp1VAZpML1tPbOEceE3-GsmX0OCl"
 
 # SIZNING GEMINI API KALITINGIZ
-GEMINI_KEY = "AIzaSyB62MjBDMf1SB1iQVoe52RIywT8KtiQ7h0"
+GEMINI_KEY = "AIzaSyB62MjBDMf1SlqVoe52RIywT8Ktiq7h0"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 ai_client = genai.Client(api_key=GEMINI_KEY)
 
-# Server o'chib qolmasligi uchun soxta port ochamiz
-from flask import Flask
 app = Flask('')
 
 @app.route('/')
@@ -23,28 +22,28 @@ def home():
 def run():
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
-print("🤖 Telegram AI Bot muvaffaqiyatli ishga tushdi...")
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Salom! Men Universal AI botman. Menga xohlagan savolingizni bering, javob berishga harakat qilaman! 🚀")
+    bot.reply_to(message, "Salom! Men Mirvosit AI botiman. Menga istalgan savolingizni bering.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    user_text = message.text
-    bot.send_chat_action(message.chat.id, 'typing')
     try:
+        user_text = message.text
+        # Kengaytirilgan va tezkor yangi model versiyasi
         response = ai_client.models.generate_content(
             model='gemini-2.5-flash',
             contents=user_text,
         )
         bot.reply_to(message, response.text)
     except Exception as e:
-        bot.reply_to(message, "Xatolik yuz berdi.")
         print(f"Xatolik: {e}")
+        bot.reply_to(message, "Xatolik yuz berdi. Birozdan so'ng qayta urinib ko'ring.")
 
-if name == "main":
+if __name__=="__main__":
     # Serverni alohida oqimda yoqamiz
     t = Thread(target=run)
     t.start()
+    
+    print("🤖 Telegram AI Bot muvaffaqiyatli ishga tushdi...")
     bot.infinity_polling()
